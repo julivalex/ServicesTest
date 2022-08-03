52ed117eae8105190491e3bb1ffe70999e7e3ebe
yuliaivanenko <y-ivanenko@alfabank.ru>
2022-08-03 20:11:51 +0300

    Simple service + add coroutine

--- app/src/main/java/com/example/servicestest/MyService.kt
+++ app/src/main/java/com/example/servicestest/MyService.kt
@@ -5,8 +5,11 @@ import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.*
 
 class MyService : Service() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate() {
         super.onCreate()
@@ -15,15 +18,18 @@ class MyService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         log("onStartCommand")
-        for (i in 0 until 100) {
-            Thread.sleep(1000)
-            log("Timer $i")
+        coroutineScope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer $i")
+            }
         }
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        coroutineScope.cancel()
         log("onDestroy")
     }
 
